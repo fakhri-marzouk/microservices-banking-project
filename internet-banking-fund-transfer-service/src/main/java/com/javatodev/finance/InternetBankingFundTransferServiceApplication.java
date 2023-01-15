c4fa30b82138ed38fcf263a4f9e7a412eb5a8d9d
fakhri <fakhrimarzouki199@gmail.com>
2023-01-15 22:46:16 +0100

    first commit

---
+++ internet-banking-fund-transfer-service/src/main/java/com/javatodev/finance/InternetBankingFundTransferServiceApplication.java
@@ -0,0 +1,32 @@
+package com.javatodev.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+@Configuration
+@EnableSwagger2
+
+public class InternetBankingFundTransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetBankingFundTransferServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.javatodev.finance")).build();
+    }
+
+}
